3c28c65f88b9cac49a9a99c4a41fdbfe41e9699a
Анатолий Беликов <Arthas.96@yandex.ru>
2026-08-21 01:34:01 +0300

    Добавлен tests.yml для ревьюера

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -110,7 +110,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = '/app/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
