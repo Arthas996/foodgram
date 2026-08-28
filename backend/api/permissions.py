0a4706db5868289f0e0191aaf4088c12c86276a8
Анатолий Беликов <Arthas.96@yandex.ru>
2026-08-29 02:14:51 +0300

    Правки

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,8 +1,9 @@
 from rest_framework.permissions import BasePermission
 
+
 class IsAuthorOrReadOnly(BasePermission):
     """Разрешает редактирование/удаление только автору объекта."""
-    
+
     def has_object_permission(self, request, view, obj):
         if request.method in ('GET', 'HEAD', 'OPTIONS'):
             return True
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -92,8 +92,6 @@ DATABASES = {
     }
 }
 
-
-
 LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
